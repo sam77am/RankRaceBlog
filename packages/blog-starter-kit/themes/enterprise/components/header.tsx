8ea362d7f4551e7f1558b589a8935692bfe3c2d5
sam77am <122092403+sam77am@users.noreply.github.com>
2025-06-25 21:22:24 +0530

    Update header.tsx

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -99,7 +99,7 @@ export const Header = () => {
 				</div>
 				<div className="col-span-2 flex flex-row items-center justify-end gap-5 text-slate-300 lg:col-span-3">
 					<nav className="hidden lg:block">{navList}</nav>
-					<Button href={baseUrl} as="a" type="primary" label="Book a demo" />
+					<Button href="https://rankrace.com/#contact" as="a" type="primary" label="Contact Us" />
 				</div>
 			</Container>
 			<div className="mt-5 flex justify-center lg:hidden">
