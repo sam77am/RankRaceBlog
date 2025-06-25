8bcff2ec28df9ff5bf414fc2e46ccc5f2ce9d09d
sam77am <122092403+sam77am@users.noreply.github.com>
2025-06-25 21:15:37 +0530

    Update footer.tsx

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -6,13 +6,14 @@ import { SocialLinks } from './social-links';
 export const Footer = () => {
 	const { publication } = useAppContext();
 	const PUBLICATION_LOGO = publication.preferences.logo;
+
 	return (
-		<footer className="border-t py-20 dark:border-neutral-800 ">
+		<footer className="border-t py-20 dark:border-neutral-800">
 			<Container className="px-5">
 				{PUBLICATION_LOGO ? (
 					<div className="mb-20 flex w-full flex-row justify-center">
 						<Link
-							href={'/'}
+							href="/"
 							aria-label={`${publication.title} home page`}
 							className="flex flex-row items-center gap-5"
 						>
@@ -24,120 +25,105 @@ export const Footer = () => {
 						{publication.title}
 					</p>
 				)}
+
 				<div className="grid w-full grid-cols-3 gap-5 md:grid-cols-6 lg:grid-cols-5">
 					<div className="col-span-1 grid grid-cols-4 gap-5 md:col-span-4 lg:col-span-3">
+						{/* Stay in touch */}
 						<div className="col-span-full md:col-span-2 lg:col-span-1">
 							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">
 								Stay in touch
 							</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#contact" className="hover:underline">
 										Contact us
 									</a>
 								</li>
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#contact" className="hover:underline">
 										Book a demo
 									</a>
 								</li>
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/blog/rss.xml" className="hover:underline">
 										Newsletter
-									</a>
-								</li>
-								<li>
-									<a href="#" className="hover:underline">
-										Slack
 									</a>
 								</li>
 							</ul>
 						</div>
+
+						{/* Resources */}
 						<div className="col-span-full md:col-span-2 lg:col-span-1">
 							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Resources</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<a href="#" className="hover:underline">
-										Community
+									<a href="https://rankrace.com/blog" className="hover:underline">
+										Blog
 									</a>
 								</li>
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#services" className="hover:underline">
 										Use Cases
 									</a>
 								</li>
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://github.com/rankraceseo" className="hover:underline">
 										Source Code
-									</a>
-								</li>
-								<li>
-									<a href="#" className="hover:underline">
-										Blog
 									</a>
 								</li>
 							</ul>
 						</div>
+
+						{/* Product */}
 						<div className="col-span-full md:col-span-2 lg:col-span-1">
 							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Product</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#pricing" className="hover:underline">
 										Pricing
 									</a>
 								</li>
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#faq" className="hover:underline">
 										Documentation
 									</a>
 								</li>
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#process" className="hover:underline">
 										Integrations
 									</a>
 								</li>
 								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#contact" className="hover:underline">
 										Support
 									</a>
 								</li>
 							</ul>
 						</div>
+
+						{/* About */}
 						<div className="col-span-1">
-							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Other links</p>
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">About</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<a href="#" className="hover:underline">
-										Events
-									</a>
-								</li>
-								<li>
-									<a href="#" className="hover:underline">
-										Careers
-									</a>
-								</li>
-								<li>
-									<a href="#" className="hover:underline">
-										Newsroom
-									</a>
-								</li>
-								<li>
-									<a href="#" className="hover:underline">
+									<a href="https://rankrace.com/#about" className="hover:underline">
 										About us
 									</a>
 								</li>
 							</ul>
 						</div>
 					</div>
+
+					{/* Right Column */}
 					<div className="col-span-2 flex flex-col items-end gap-5 text-right text-slate-600 dark:text-neutral-300 md:text-left">
 						<SocialLinks />
-						<p>&copy; 2023 Company Inc.</p>
+						<p>&copy; {new Date().getFullYear()} RankRace</p>
 						<p>
-							<a href="#" className="hover:underline">
+							<a href="https://rankrace.com/privacy-policy" className="hover:underline">
 								Privacy Policy
 							</a>{' '}
 							·{' '}
-							<a href="#" className="hover:underline">
+							<a href="https://rankrace.com/terms-of-service" className="hover:underline">
 								Terms
 							</a>
 						</p>
